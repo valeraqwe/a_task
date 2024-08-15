b9f5e03c9475e93a8821f4ece2d2299748be792b
Valeriy <valera.komar.1999@gmail.com>
2024-08-15 12:11:24 +0300

    adding caching

--- app/Services/TVMazeService.php
+++ app/Services/TVMazeService.php
@@ -2,22 +2,26 @@
 
 namespace App\Services;
 
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 
 class TVMazeService
 {
     public function searchTVMaze(string $query): array
     {
-        $response = Http::get("http://api.tvmaze.com/search/shows", ['q' => $query]);
+        // Cache the result for 60 minutes
+        return Cache::remember("tvmaze_search_{$query}", 60, function () use ($query) {
+            $response = Http::get("http://api.tvmaze.com/search/shows", ['q' => $query]);
 
-        if ($response->failed()) {
-            return ['error' => 'Failed to fetch data from TVMaze'];
-        }
+            if ($response->failed()) {
+                return ['error' => 'Failed to fetch data from TVMaze'];
+            }
 
-        $results = collect($response->json())->filter(function ($show) use ($query) {
-            return strcasecmp($show['show']['name'], $query) === 0;
+            $results = collect($response->json())->filter(function ($show) use ($query) {
+                return strcasecmp($show['show']['name'], $query) === 0;
+            });
+
+            return $results->values()->toArray();
         });
-
-        return $results->values()->toArray();
     }
 }
